9b9728b97385f07e21b9e29a11b37d7b9048ff2a
Wencan Luo <wencanluo.cn@gmail.com>
2013-08-01 15:31:44 -0700

    Add Resources

---
+++ doc/Machine Learning Model.mm
@@ -0,0 +1,106 @@
+<map version="1.0.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1368295364143" ID="ID_1097755583" MODIFIED="1368295525972" STYLE="bubble" TEXT="Machine Learning Model">
+<node CREATED="1368295375349" ID="ID_1929208633" LINK="http://en.wikipedia.org/wiki/Deep_learning" MODIFIED="1368980626115" POSITION="right" STYLE="bubble" TEXT="Deep Learning">
+<node CREATED="1368295785865" ID="ID_1429942076" MODIFIED="1368295789273" TEXT="introducation">
+<node CREATED="1368295837332" ID="ID_1258013865" LINK="http://www.deeplearning.net/tutorial/" MODIFIED="1368295842855" TEXT="Tutorial"/>
+<node CREATED="1369153787358" ID="ID_930417696" LINK="http://ufldl.stanford.edu/wiki/index.php/UFLDL_Tutorial" MODIFIED="1369153795624" TEXT="Unsupervised Feature learning">
+<node CREATED="1369153478702" ID="ID_700288094" LINK="http://deeplearning.net/reading-list/tutorials/" MODIFIED="1369153493405" TEXT="Tutorial2"/>
+<node CREATED="1369244446821" ID="ID_596003283" LINK="https://github.com/dkyang/UFLDL-Tutorial-Exercise" MODIFIED="1369244453458" TEXT="code"/>
+</node>
+<node CREATED="1369154546249" ID="ID_1738457774" MODIFIED="1369154549014" TEXT="Survey">
+<node CREATED="1369154572186" ID="ID_1123362371" LINK="http://www.iro.umontreal.ca/~lisa/pointeurs/TR1312.pdf" MODIFIED="1369154623795" TEXT="Bengio2009"/>
+<node CREATED="1369154575624" ID="ID_513881951" LINK="http://arxiv.org/pdf/1206.5538.pdf  " MODIFIED="1369154587467" TEXT="Bengio2012"/>
+</node>
+</node>
+<node CREATED="1368295794015" ID="ID_1016824014" MODIFIED="1368295800003" TEXT="Implementation">
+<node CREATED="1368295761285" ID="ID_258311048" LINK="http://deeplearning.net/software/theano/" MODIFIED="1369248660540" TEXT="Theano">
+<node CREATED="1368295721081" ID="ID_1182801503" LINK="http://deeplearning.net/software/theano/tutorial/index.html#tutorial" MODIFIED="1369261136872" TEXT="Tutorial"/>
+<node CREATED="1369248666890" ID="ID_1440971390" MODIFIED="1369248668856" TEXT="Install">
+<node CREATED="1369248746698" ID="ID_200641506" LINK="http://sourceforge.net/projects/mingw/files/" MODIFIED="1369248756450" TEXT="MinGW"/>
+<node CREATED="1369248671914" ID="ID_190195897" LINK="https://zignar.net/2012/06/17/install-python-on-windows/" MODIFIED="1369248715258" TEXT="pip"/>
+<node CREATED="1369248778340" ID="ID_483767581" MODIFIED="1369248783442" TEXT="Install NumPy then SciPy">
+<node CREATED="1369248793725" ID="ID_417817937" MODIFIED="1369248800262" TEXT="pip install numpy scipy"/>
+</node>
+<node CREATED="1369248812573" ID="ID_1569705230" LINK="http://deeplearning.net/software/theano/install.html#windows" MODIFIED="1369248826225" TEXT="Installing Theano"/>
+</node>
+<node CREATED="1368295773363" ID="ID_1724079046" LINK="https://github.com/lisa-lab/DeepLearningTutorials" MODIFIED="1368295776856" TEXT="code"/>
+</node>
+</node>
+<node CREATED="1368740364816" ID="ID_26682810" MODIFIED="1368740367219" TEXT="Usage">
+<node CREATED="1368740368186" ID="ID_1221615698" MODIFIED="1368740369730" TEXT="Parsing">
+<node CREATED="1368740370760" ID="ID_1203224673" LINK="http://ronan.collobert.com/pub/matos/2011_parsing_aistats.pdf" MODIFIED="1368740389198" TEXT="Collobert,2010"/>
+<node CREATED="1368740500217" ID="ID_1057922035" LINK="http://ronan.collobert.com/pub/matos/2011_nlp_jmlr.pdf" MODIFIED="1368740515024" TEXT="Collobert,at el. 2011"/>
+<node CREATED="1369153109733" ID="ID_1707950816" LINK="http://www.hds.utc.fr/~bordesan/dokuwiki/lib/exe/fetch.php?id=en%3Apubli&amp;cache=cache&amp;media=en:bordes12aistats.pdf" MODIFIED="1369153136655" TEXT="Bordes, 2012"/>
+</node>
+<node CREATED="1368979459012" ID="ID_1722333223" MODIFIED="1368979461820" TEXT="NLP">
+<node CREATED="1368979525372" ID="ID_1537138182" LINK="http://ronan.collobert.com/pub/matos/2008_nlp_icml.pdf" MODIFIED="1368979537125" TEXT="Collobert, 2008"/>
+</node>
+</node>
+<node CREATED="1368981097388" ID="ID_1914141655" MODIFIED="1368981109136" TEXT="Papers">
+<node CREATED="1368979525372" ID="ID_789605918" LINK="http://ronan.collobert.com/pub/matos/2008_nlp_icml.pdf" MODIFIED="1368979537125" TEXT="Collobert, 2008"/>
+<node CREATED="1369153575077" ID="ID_348476793" LINK="http://www.iro.umontreal.ca/~lisa/pointeurs/TR1312.pdf" MODIFIED="1369153597030" TEXT="Bengio, 2009"/>
+</node>
+<node CREATED="1375119269118" ID="ID_1462305756" MODIFIED="1375119272011" TEXT="Model">
+<node CREATED="1375119480535" ID="ID_222311404" MODIFIED="1375119486515" TEXT="Word Representation">
+<node CREATED="1375119273086" ID="ID_1709194030" MODIFIED="1375119287484" TEXT="One-hot Representation"/>
+<node CREATED="1375119299071" ID="ID_1238732064" MODIFIED="1375119299548" TEXT="Distributed Representation">
+<node CREATED="1375119540378" ID="ID_96173828" LINK="http://www.cs.toronto.edu/~hinton/absps/families.pdf" MODIFIED="1375119780477" TEXT="Hinton 1986">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1375120654863" ID="ID_124002309" LINK="http://www.gatsby.ucl.ac.uk/~amnih/papers/threenew.pdf" MODIFIED="1375120681058" TEXT="Mnih&amp;Hinton 2007"/>
+<node CREATED="1375121573638" ID="ID_1469583810" LINK="http://metaoptimize.com/projects/wordreprs/" MODIFIED="1375121592586" TEXT="C&amp;W - Turian"/>
+<node CREATED="1375121654406" ID="ID_1710461421" LINK="http://www.socher.org/index.php/Main/ImprovingWordRepresentationsViaGlobalContextAndMultipleWordPrototypes" MODIFIED="1375121667175" TEXT="Huang 2012"/>
+</node>
+<node CREATED="1375119736912" ID="ID_1382703782" MODIFIED="1375119740797" TEXT="Curse of Dimensionality">
+<node CREATED="1375119747153" ID="ID_366237321" LINK="http://machinelearning.wustl.edu/mlpapers/paper_files/BengioDVJ03.pdf" MODIFIED="1375119783084" TEXT="Bengio 2003">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node CREATED="1375121138308" ID="ID_761856854" MODIFIED="1375121139345" TEXT="RNNLM ">
+<node CREATED="1375121140948" ID="ID_1788460412" LINK="http://www.fit.vutbr.cz/~imikolov/rnnlm/thesis.pdf" MODIFIED="1375121178904" TEXT="Mikolov thesis">
+<node CREATED="1375121624231" ID="ID_478726592" LINK="http://www.fit.vutbr.cz/~imikolov/rnnlm/" MODIFIED="1375121630872" TEXT="RNNLM Toolkit"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1368295516657" ID="ID_712356784" MODIFIED="1369350343270" POSITION="right" TEXT="LDA">
+<node CREATED="1368120789968" ID="ID_807962228" MODIFIED="1368742897785" TEXT="MALLET">
+<icon BUILTIN="idea"/>
+<node CREATED="1368135319030" ID="ID_1603223156" LINK="http://mallet.cs.umass.edu/" MODIFIED="1368135326480" TEXT="download"/>
+<node CREATED="1368135404079" ID="ID_6780781" LINK="http://mallet.cs.umass.edu/mallet-tutorial.pdf" MODIFIED="1368135407820" TEXT="Tutorial"/>
+</node>
+</node>
+<node CREATED="1368295531429" ID="ID_827233675" MODIFIED="1369157378543" POSITION="right" TEXT="Graph Model">
+<node CREATED="1368296646037" ID="ID_393727302" MODIFIED="1368296647043" TEXT="HITS">
+<node CREATED="1368660133748" ID="ID_169393622" LINK="#ID_954761480" MODIFIED="1368660284634" TEXT="Jung"/>
+</node>
+<node CREATED="1368296653093" ID="ID_1889553075" MODIFIED="1368296653725" TEXT="PageRank">
+<node CREATED="1368659156217" ID="ID_1027041952" LINK="http://www.ams.org/samplings/feature-column/fcarc-pagerank" MODIFIED="1368659164524" TEXT="Introduction"/>
+<node CREATED="1368659508328" ID="ID_1409270021" LINK="http://www.cemam.usj.edu.lb/uirws/ws-09-00048.pdf" MODIFIED="1368659511107" TEXT="Paper"/>
+<node CREATED="1368660152942" ID="ID_769171067" MODIFIED="1368660156407" TEXT="Implementation">
+<node CREATED="1368660131080" ID="ID_299788436" MODIFIED="1368660132827" TEXT="Java">
+<node CREATED="1368660133748" ID="ID_954761480" LINK="http://jung.sourceforge.net/" MODIFIED="1368660271728" TEXT="Jung"/>
+</node>
+<node CREATED="1368660160573" ID="ID_1728091637" MODIFIED="1368660163647" TEXT="Python">
+<node CREATED="1368660165348" ID="ID_1921807199" LINK="https://github.com/louridas/pagerank/tree/master/python" MODIFIED="1368660211274" TEXT="louridas"/>
+</node>
+</node>
+</node>
+<node CREATED="1369157380571" ID="ID_279110520" MODIFIED="1369173145285" TEXT="EdgeRank">
+<node CREATED="1369157387257" ID="ID_563654578" LINK="http://edgerank.net/" MODIFIED="1369157393536" TEXT="Introduction"/>
+<node CREATED="1369157404398" ID="ID_1805599903" LINK="http://anuary.com/43/facebook-friend-rank-algorithm" MODIFIED="1369157411781" TEXT="Facebook Friend Rank"/>
+</node>
+<node CREATED="1369355116796" ID="ID_1081145496" MODIFIED="1369355119336" TEXT="BayesNet"/>
+</node>
+<node CREATED="1368296788900" ID="ID_47614271" LINK="https://code.google.com/p/crfpp/" MODIFIED="1369862974945" POSITION="right" TEXT="CRF"/>
+<node CREATED="1368742843282" ID="ID_168465434" MODIFIED="1368742848071" POSITION="right" TEXT="Integer Linear Programming ">
+<node CREATED="1368742856698" ID="ID_1749148966" LINK="http://lpsolve.sourceforge.net/5.5/" MODIFIED="1368742869972" TEXT="lp_solve"/>
+</node>
+<node CREATED="1369171665529" ID="ID_1464909267" MODIFIED="1369171669290" POSITION="right" TEXT="Ranking Model">
+<node CREATED="1369172677321" ID="ID_382503944" LINK="http://drona.csa.iisc.ernet.in/~shivani/Events/SDM-10-Tutorial/sdm10-tutorial.pdf" MODIFIED="1369172686920" TEXT="Tutorial"/>
+<node CREATED="1369172115341" ID="ID_1960109188" LINK="https://code.google.com/p/jforests/" MODIFIED="1369172121053" TEXT="Tree Ensembles"/>
+</node>
+</node>
+</map>
---
+++ doc/competition.mm
@@ -0,0 +1,9 @@
+<map version="1.0.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1369174015529" ID="ID_853415810" MODIFIED="1369176408062" STYLE="bubble" TEXT="competition">
+<node CREATED="1369174042559" ID="ID_1366859991" LINK="http://www.kaggle.com/competitions" MODIFIED="1369174067774" POSITION="right" STYLE="bubble" TEXT="Kaggle"/>
+<node CREATED="1369174441283" ID="ID_1963580316" LINK="http://en.wikipedia.org/wiki/Loebner_Prize" MODIFIED="1369174448328" POSITION="right" TEXT="Loebner Prize">
+<node CREATED="1369174577759" ID="ID_1357627694" LINK="http://www.loebner.net/Prizef/loebner-prize.html" MODIFIED="1369174580001" TEXT="2013"/>
+</node>
+</node>
+</map>
