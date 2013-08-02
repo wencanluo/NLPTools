905a5e0c9c63345d3cb3e30d48d801776adcda4d
Wencan Luo <wencanluo.cn@gmail.com>
2013-08-02 13:31:48 -0700

    Updated Lemma interface

--- python/Lemma.py
+++ python/Lemma.py
@@ -12,7 +12,29 @@ from nltk.corpus import wordnet as wn
 from nltk.stem.wordnet import WordNetLemmatizer
 lmtzr = WordNetLemmatizer()
 
+def lemmatizeSentence(sentence):
+	"""
+	@Function: Lemmatize a sentence. 
+	@param sentence: string, a string of words (splity by " ") 
+	@Example:
+		 "finding friend location"=lemmatizeSentence("finding friends locations")
+	"""
+	
+	global lmtzr
+	ls = ""
+	for word in sentence.split(): 
+		word = lmtzr.lemmatize(word)
+		ls = ls + word + " "
+	return ls.strip()
+
 def lemmatize(word):
+	"""
+	@Function: Lemmatize a word. 
+	@param word: string, the input word 
+	@Example:
+		 friend=lemmatize("friends")
+	"""
+	
 	global lmtzr
 	lemma = lmtzr.lemmatize(word)
 	return lemma
@@ -25,4 +47,6 @@ if __name__ == "__main__":
 	for i in range(len(Ts)):
 		print lemmatize(Ts[i])
 	
+	print lemmatize("said")
+	print lemmatizeSentence("finding friends locations")
 	print "Done!"
